b39187f9069f13c10e16e5cb89d1230c0f080692
nitin06890 <57109530+nitin06890@users.noreply.github.com>
2022-08-22 23:40:35 +0530

    Equal sum partition

---
+++ GFG/DP/Equal sum partition.cpp
@@ -0,0 +1,49 @@
+// OJ: https://practice.geeksforgeeks.org/problems/subset-sum-problem2014/1
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<vector<int>> dp;
+
+    bool isSubsetSum(int arr[], int sum, int n)
+    {
+        if (sum == 0)
+            return 1;
+        dp.resize(n + 1, vector<int>(sum + 1));
+        for (int i = 0; i < n + 1; i++)
+        {
+            for (int j = 0; j < sum + 1; j++)
+            {
+                if (i == 0)
+                    dp[i][j] = 0;
+                if (j == 0)
+                    dp[i][j] = 1;
+            }
+        }
+        for (int i = 1; i < n + 1; i++)
+        {
+            for (int j = 1; j < sum + 1; j++)
+            {
+                if (arr[i - 1] <= j)
+                    dp[i][j] = dp[i - 1][j - arr[i - 1]] || dp[i - 1][j];
+                else
+                    dp[i][j] = dp[i - 1][j];
+            }
+        }
+        return dp[n][sum];
+    }
+
+    int equalPartition(int n, int arr[])
+    {
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+            sum += arr[i];
+        if (sum % 2 != 0)
+            return false;
+        else if (sum % 2 == 0)
+            return isSubsetSum(arr, sum / 2, n);
+    }
+};
