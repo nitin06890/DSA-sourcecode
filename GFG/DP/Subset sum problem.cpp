679cb3cb6c456b3147a0f778a947c99e611eda30
nitin06890 <57109530+nitin06890@users.noreply.github.com>
2022-08-23 00:15:55 +0530

    subset sum

--- GFG/DP/Subset sum problem.cpp
+++ GFG/DP/Subset sum problem.cpp
@@ -47,15 +47,10 @@ public:
         int n = arr.size();
         dp.resize(n + 1, vector<int>(sum + 1));
         for (int i = 0; i < n + 1; i++)
-        {
-            for (int j = 0; j < sum + 1; j++)
-            {
-                if (i == 0)
-                    dp[i][j] = 0;
-                if (j == 0)
-                    dp[i][j] = 1;
-            }
-        }
+            dp[i][0] = 0;
+        for (int j = 0; j < n + 1; j++)
+            dp[0][j] = 1;
+
         for (int i = 1; i < n + 1; i++)
         {
             for (int j = 1; j < sum + 1; j++)
