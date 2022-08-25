2f846686692161447acbac058f9615757cfde0d3
nitin06890 <57109530+nitin06890@users.noreply.github.com>
2022-08-25 20:17:49 +0530

    lcs

--- GFG/DP/lcs.cpp
+++ GFG/DP/lcs.cpp
@@ -44,7 +44,6 @@ public:
         {
             for (int j = 1; j < y + 1; j++)
             {
-                dp[i][j] = 0;
                 if (s1[i - 1] == s2[j - 1])
                     dp[i][j] = 1 + dp[i - 1][j - 1];
                 else
