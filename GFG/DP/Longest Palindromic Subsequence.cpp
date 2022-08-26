07f2c37c298787d880c777751ec83134cb47ae27
nitin06890 <57109530+nitin06890@users.noreply.github.com>
2022-08-26 18:34:10 +0530

    Longest Palindromic Subsequence

---
+++ GFG/DP/Longest Palindromic Subsequence.cpp
@@ -0,0 +1,31 @@
+// OJ: https://practice.geeksforgeeks.org/problems/longest-palindromic-subsequence-1612327878/1
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+    int lcs(string &X, string &Y, int m, int n)
+    {
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        for (int i = 1; i < m + 1; i++)
+        {
+            for (int j = 1; j < n + 1; j++)
+            {
+                if (X[i - 1] == Y[j - 1])
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[m][n];
+    }
+
+public:
+    int longestPalinSubseq(string A)
+    {
+        string B = string(A.rbegin(), A.rend());
+        // reverse(B.begin(), B.end());
+        return (A, B, A.length(), B.length());
+    }
+};
